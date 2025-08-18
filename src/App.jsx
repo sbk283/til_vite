2fb19e5a8f0289fd6416490f29610371f4f3aac0
sbkcoding <sbkcoding@gmail.com>
2025-08-18 17:01:57 +0900

    [docs] 컨벤션 환경설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div>App</div>;
+  const [count, setCount] = useState(0);
+  const [q, setQ] = useState("hello");
+  useEffect(() => {
+    console.log(q);
+  }, []);
+  return (
+    <div>
+      <h1>App</h1>
+      <button onClick={() => setCount(count + 1)}>count: {count}</button>
+      <div>
+        <input value={q} onChange={e => setQ(e.target.value)} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
